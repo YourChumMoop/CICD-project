c340d87e08afc047fb70e9d06d86c3e638fd8631
Matt Oberdalhoff <reliable.vagabond@gmail.com>
2025-02-06 14:24:13 -0600

    attempt to resolve possibly undefined issue

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,7 +3,11 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
+    const model = models[modelName];
+    if(!model) {
+      throw new Error('model does not exist!');
+    }
+    let modelExists = await model.db.db.listCollections({
       name: collectionName
     }).toArray()
 
